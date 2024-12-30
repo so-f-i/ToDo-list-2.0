a99adc845156f26469f01f6b7d568d8e4c568e80
so-f-i <sonya.mavlenko.02@gmail.com>
2024-12-30 14:37:23 +0300

    Create exceptions.py

---
+++ exceptions.py
@@ -0,0 +1,7 @@
+class TaskError(Exception):
+    """Исключение, связанное с ошибками в задачах."""
+    pass
+
+class TodoListError(Exception):
+    """Исключение, связанное с ошибками в списке задач."""
+    pass
